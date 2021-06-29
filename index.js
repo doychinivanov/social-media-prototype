4765d3edb7a89df97cc4f4fabf1543b83dae1da2
Doychin Ivanov <doychinivn@gmail.com>
2021-06-29 19:05:04 +0300

    configs expanded and some controllers added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {PORT} = require('./config/index');
 const database = require('./config/databaseConfig');
 const expressConfig = require('./config/expressConfig');
+const router = require('./config/routes');
 
 startApplication();
 
@@ -9,8 +10,7 @@ async function startApplication(){
     const app = express();
     await database(app);
     expressConfig(app);
-    
-    app.get('/', (req, res)=> res.send('Hi'))
+    router(app);
 
     app.listen(PORT, () => console.log(`Server listening ong port ${PORT}...`));
 }
