5173c658c3d815b2e53d76102926c2f272090413
Doychin Ivanov <doychinivn@gmail.com>
2021-06-30 11:55:42 +0300

    input forms progress update; controllers read req.body correctlly

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 
 router.get('/register', (req, res)=>{
-    res.send('register');
+    res.render('authViews/register');
+});
+
+router.post('/register', (req,res)=>{
+    console.log(req.body);
 });
 
 router.get('/login', (req, res)=>{
-    res.send('login');
+    res.render('authViews/login');
+});
+
+router.post('/login', (req,res)=>{
+    console.log(req.body);
 });
 
 module.exports = router;
