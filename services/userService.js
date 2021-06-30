9e747142b2324ae664c9568292edf3aa7b774ba3
doychinivanov <doychinivn@gmail.com>
2021-06-30 13:05:01 +0300

    user service added

---
+++ services/userService.js
@@ -0,0 +1,30 @@
+const User = require('../models/User');
+
+async function createUser(email, username, hashedPassword, birthDay, isPrivate){
+    const user = new User({
+        email,
+        username,
+        hashedPassword,
+        birthDay: birthDay || null,
+        isPrivate: isPrivate || false,
+        posts: [],
+        following: [],
+        followers: []
+    });
+
+    await user.save();
+
+    return user;
+};
+
+async function getUserByUsername(username){
+    const pattern = new RegExp(`^${username}$`, 'i')
+    const user = await User.findOne({username: {$regex: pattern}});
+
+    return user;
+}
+
+module.exports = {
+    createUser,
+    getUserByUsername
+}
