8a07faa44294935592c11fb48b063739c198058b
doychinivanov <doychinivn@gmail.com>
2021-08-01 08:03:07 +0300

    deployment update

--- static/js/api/api.js
+++ static/js/api/api.js
@@ -1,12 +1,12 @@
 export async function getCommentsByPostId(postId){
-    const data = await fetch(process.env.PORT + '/comments/' + postId);
+    const data = await fetch('https://toggle-social-media.herokuapp.com/comments/' + postId);
     const comments = await data.json();
 
     return comments;
 }
 
 export async function createComment(postId, body){
-    return await fetch(process.env.PORT + '/comments/create/' + postId, {
+    return await fetch('https://toggle-social-media.herokuapp.com/comments/create/' + postId, {
      method: 'post',
      headers: {'Content-Type' : 'application/json'},
      body: JSON.stringify(body)
@@ -14,18 +14,18 @@ export async function createComment(postId, body){
 };
 
 export async function delComment(commentId){
-    return await fetch(process.env.PORT + '/comments/delete/' + commentId, {method: 'delete'});
+    return await fetch('https://toggle-social-media.herokuapp.com/comments/delete/' + commentId, {method: 'delete'});
 }
 
 export async function getFollowers(userId){
-    const response = await fetch(process.env.PORT + '/user/followers/' + userId);
+    const response = await fetch('https://toggle-social-media.herokuapp.com/user/followers/' + userId);
     const followers = await response.json();
 
     return followers;
 }
 
 export async function getFollowing(userId){
-    const response = await fetch(process.env.PORT + '/user/following/' + userId);
+    const response = await fetch('https://toggle-social-media.herokuapp.com/user/following/' + userId);
     const following = await response.json();
 
     return following;
