131a60872907b585603711434d1c3130876a2666
doychinivanov <doychinivn@gmail.com>
2021-08-01 07:48:51 +0300

    updated ajax url

--- config/index.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-    DB_CONNECTION_STRING: 'mongodb+srv://doychin:perm753951@toggle-social-media.1ca5l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    COOKIE_NAME: 'AUTH_SESSION_COOKIE',
-    COOKIE_ERROR: 'CURR_ERR_MSG',
-    TOKEN_SECRET: 'fgop_113-kZZkmasd5196_55ahk2!4!'
-};
--- static/js/api/api.js
+++ static/js/api/api.js
@@ -1,12 +1,12 @@
 export async function getCommentsByPostId(postId){
-    const data = await fetch('http://localhost:3000/comments/' + postId);
+    const data = await fetch(process.env.PORT + '/comments/' + postId);
     const comments = await data.json();
 
     return comments;
 }
 
 export async function createComment(postId, body){
-    return await fetch('http://localhost:3000/comments/create/' + postId, {
+    return await fetch(process.env.PORT + '/comments/create/' + postId, {
      method: 'post',
      headers: {'Content-Type' : 'application/json'},
      body: JSON.stringify(body)
@@ -14,18 +14,18 @@ export async function createComment(postId, body){
 };
 
 export async function delComment(commentId){
-    return await fetch('http://localhost:3000/comments/delete/' + commentId, {method: 'delete'});
+    return await fetch(process.env.PORT + '/comments/delete/' + commentId, {method: 'delete'});
 }
 
 export async function getFollowers(userId){
-    const response = await fetch('http://localhost:3000/user/followers/' + userId);
+    const response = await fetch(process.env.PORT + '/user/followers/' + userId);
     const followers = await response.json();
 
     return followers;
 }
 
 export async function getFollowing(userId){
-    const response = await fetch('http://localhost:3000/user/following/' + userId);
+    const response = await fetch(process.env.PORT + '/user/following/' + userId);
     const following = await response.json();
 
     return following;
